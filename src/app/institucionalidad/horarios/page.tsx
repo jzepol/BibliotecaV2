84499a8fd0272b1cda9eff2ad6f8b2534494ffa2
Jzepol <jzepol94@gmail.com>
2025-05-11 01:36:25 -0300

    update horarios
    update horarios

--- src/app/institucionalidad/horarios/page.tsx
+++ src/app/institucionalidad/horarios/page.tsx
@@ -1,10 +1,23 @@
 'use client'
 
+import '@/styles/HorariosPage.css'
 
+export default function HorariosPage() {
+  return (
+    <div className="horarios-container">
+      <h1>Horarios de Atención</h1>
 
-export default function Horarios() {
-    return (
-      <div className="rincon-julia-container">
-        <h1>Nuestros horarios</h1>
-    </div>)
+      <div className="horario-bloque">
+        <h2>Mañana</h2>
+        <p>Lunes y Martes</p>
+        <p>10:00 a 13:00 hs</p>
+      </div>
+
+      <div className="horario-bloque">
+        <h2>Tarde</h2>
+        <p>Lunes, Martes, Miércoles, Jueves y Viernes</p>
+        <p>17:00 a 20:00 hs</p>
+      </div>
+    </div>
+  )
 }
