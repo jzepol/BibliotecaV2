f6bf3b530bb7d44b330375d3834e4f001ec4808a
Jzepol <jzepol94@gmail.com>
2025-05-27 01:16:33 -0300

    update2
    eliminacion prisma migrations

--- prisma/migrations/20250402233142_agregar_estado_eventos/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "EstadoEvento" AS ENUM ('PROXIMAMENTE', 'EN_CURSO', 'FINALIZADO');
-
--- AlterTable
-ALTER TABLE "Evento" ADD COLUMN     "estado" "EstadoEvento" NOT NULL DEFAULT 'PROXIMAMENTE';
